9ecdaffb4c3b241b32983582dbd8d264f1f1a4b2
Mark Zuber <mzuber@microsoft.com>
2019-03-13 14:51:59 -0700

    add adalextensions and unit tests projects

---
+++ tests/Microsoft.Identity.AdalExtensions.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.AdalExtensions.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
