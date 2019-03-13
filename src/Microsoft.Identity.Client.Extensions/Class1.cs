489265801f6932b9b600edbf7e75d4be6463e352
Mark Zuber <mzuber@microsoft.com>
2019-03-13 11:18:06 -0700

    Create extensions skeleton

---
+++ src/Microsoft.Identity.Client.Extensions/Class1.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.Identity.Client.Extensions
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/Microsoft.Identity.Client.WebExtensions.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Client.WebExtensions.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
