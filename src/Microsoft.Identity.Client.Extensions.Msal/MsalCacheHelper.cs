748c6d664c19d4276e727f50c45794016cbe9943
James Williams <jamew@microsoft.com>
2019-04-01 18:34:10 -0700

    Fewer statics, and clear without public app (#14)
    * Update MsalCacheHelper to have fewer statics and provide a way to create a helper without registering so that users can clear the cache without a public app.
    
    * Changes from feedback
    
    * Changes from feedback

--- src/Microsoft.Identity.Client.Extensions.Msal/MsalCacheHelper.cs
+++ src/Microsoft.Identity.Client.Extensions.Msal/MsalCacheHelper.cs
@@ -14,21 +14,22 @@ namespace Microsoft.Identity.Client.Extensions.Msal
     public class MsalCacheHelper
     {
         /// <summary>
-        /// A lock object for serialization
+        /// A default logger for use if the user doesn't want to provide their own.
         /// </summary>
-        private static readonly object s_lockObject = new object();
-
         private static readonly Lazy<TraceSource> s_staticLogger = new Lazy<TraceSource>(() =>
         {
             return (TraceSource)EnvUtils.GetNewTraceSource(nameof(MsalCacheHelper) + "Singleton");
         });
 
-        private static readonly Lazy<MsalCacheStorage> s_staticStore = new Lazy<MsalCacheStorage>(() =>
-        {
-            return new MsalCacheStorage(s_storageCreationProperties, logger: s_staticLogger.Value);
-        });
+        /// <summary>
+        /// A lock object for serialization
+        /// </summary>
+        private readonly object _lockObject = new object();
 
-        private static StorageCreationProperties s_storageCreationProperties;
+        /// <summary>
+        /// Properties used to create storage on disk.
+        /// </summary>
+        private readonly StorageCreationProperties _storageCreationProperties;
 
         /// <summary>
         /// Holds a lock object when this helper is accessing the cache. Null otherwise.
@@ -48,50 +49,34 @@ namespace Microsoft.Identity.Client.Extensions.Msal
         /// <summary>
         /// Gets the token cache
         /// </summary>
-        private readonly ITokenCache _userTokenCache;
+        private ITokenCache _userTokenCache;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="MsalCacheHelper"/> class.
-        ///
-        /// FOR TESTING ONLY!
+        /// Creates a new instance of this class.
         /// </summary>
-        /// <param name="tokenCache">token cache</param>
-        /// <param name="storage">Adal cache storage</param>
-        /// <param name="logger">Logger</param>
-        internal MsalCacheHelper(ITokenCache tokenCache, MsalCacheStorage storage, TraceSource logger)
+        /// <param name="storageCreationProperties">Properties to use when creating storage on disk.</param>
+        /// <param name="logger">Passing null uses the default logger</param>
+        public MsalCacheHelper(StorageCreationProperties storageCreationProperties, TraceSource logger = null)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _store = storage ?? throw new ArgumentNullException(nameof(storage));
+            _logger = logger ?? s_staticLogger.Value;
+            _storageCreationProperties = storageCreationProperties;
+            _store = new MsalCacheStorage(_storageCreationProperties, logger);
 
-            // Normally this is set by RegisterCache, but that doesn't get called in tests.
-            s_storageCreationProperties = storage._creationProperties;
+        }
 
-            _userTokenCache = tokenCache ?? throw new ArgumentNullException(nameof(tokenCache));
+        /// <summary>
+        /// An internal constructor allowing unit tests to data explicitly rather than initializing here.
+        /// </summary>
+        /// <param name="userTokenCache">The token cache to synchronize with the backing store</param>
+        /// <param name="store">The backing store to use.</param>
+        /// <param name="logger">Passing null uses the default logger</param>
+        internal MsalCacheHelper(ITokenCache userTokenCache, MsalCacheStorage store, TraceSource logger = null)
+        {
+            _logger = logger ?? s_staticLogger.Value;
+            _store = store;
+            _storageCreationProperties = store._creationProperties;
 
-            _userTokenCache.SetBeforeAccess(BeforeAccessNotification);
-            _userTokenCache.SetAfterAccess(AfterAccessNotification);
-
-            _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Initializing adal cache");
-
-            byte[] data = _store.ReadData();
-
-            _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Read '{data?.Length}' bytes from storage");
-
-            if (data != null && data.Length > 0)
-            {
-                try
-                {
-                    _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Deserializing data into memory");
-                    _userTokenCache.DeserializeMsalV3(data);
-                }
-                catch (Exception e)
-                {
-                    _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"An exception was encountered while deserializing the data during initialization of {nameof(MsalCacheHelper)} : {e}");
-                    _store.Clear();
-                }
-            }
-
-            _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Done initializing");
+            RegisterCache(userTokenCache);
         }
 
         /// <summary>
@@ -109,32 +94,47 @@ namespace Microsoft.Identity.Client.Extensions.Msal
         /// Gets a singleton instance of the TokenCacheHelper
         /// </summary>
         /// <param name="tokenCache">Token Cache</param>
-        /// <param name="storageProperties">Properties to use when creating the storage on disk</param>
         /// <returns>token cache helper</returns>
-        public static MsalCacheHelper RegisterCache(ITokenCache tokenCache, StorageCreationProperties storageProperties)
+        public void RegisterCache(ITokenCache tokenCache)
         {
-            lock (s_lockObject)
+            lock (_lockObject)
             {
-                try
+                _userTokenCache = tokenCache ?? throw new ArgumentNullException(nameof(tokenCache));
+
+                _userTokenCache.SetBeforeAccess(BeforeAccessNotification);
+                _userTokenCache.SetAfterAccess(AfterAccessNotification);
+
+                _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Initializing msal cache");
+
+                byte[] data = _store.ReadData();
+
+                _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Read '{data?.Length}' bytes from storage");
+
+                if (data != null && data.Length > 0)
                 {
-                    s_storageCreationProperties = storageProperties;
-                    s_staticLogger.Value.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Creating '{nameof(RegisterCache)}'");
-                    return new MsalCacheHelper(tokenCache, s_staticStore.Value, s_staticLogger.Value);
+                    try
+                    {
+                        _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Deserializing data into memory");
+                        _userTokenCache.DeserializeMsalV3(data);
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.TraceEvent(TraceEventType.Warning, /*id*/ 0, $"An exception was encountered while deserializing the data during initialization of {nameof(MsalCacheHelper)} : {e}");
+
+                        Clear();
+                    }
                 }
-                catch (Exception e)
-                when (SharedUtilities.LogExceptionAndDoNotHandle(() => s_staticLogger.Value.TraceEvent(TraceEventType.Error, /*id*/ 0, $"Problem creating '{nameof(RegisterCache)}' : '{e}'")))
-                {
-                    throw;
-                }
+
+                _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Done initializing");
             }
         }
 
         /// <summary>
         /// Clears the token store
         /// </summary>
-        public static void Clear()
+        public void Clear()
         {
-            s_staticStore.Value.Clear();
+            _store.Clear();
         }
 
         /// <summary>
@@ -147,8 +147,8 @@ namespace Microsoft.Identity.Client.Extensions.Msal
 
             _logger.TraceEvent(TraceEventType.Information, /*id*/ 0, $"Acquiring lock for token cache");
             CacheLock = new CrossPlatLock(
-                Path.GetFileNameWithoutExtension(s_storageCreationProperties.CacheFileName),
-                Path.Combine(s_storageCreationProperties.CacheDirectory, s_storageCreationProperties.CacheFileName) + ".lockfile");
+                Path.GetFileNameWithoutExtension(_storageCreationProperties.CacheFileName),
+                Path.Combine(_storageCreationProperties.CacheDirectory, _storageCreationProperties.CacheFileName) + ".lockfile");
 
             if (_store.HasChanged)
             {
@@ -169,7 +169,7 @@ namespace Microsoft.Identity.Client.Extensions.Msal
                         _logger.TraceEvent(TraceEventType.Error, /*id*/ 0, $"No data found in the store, clearing the cache in memory.");
 
                         // Clear the memory cache
-                        _store.Clear();
+                        Clear();
                         throw;
                     }
                 }
@@ -211,7 +211,7 @@ namespace Microsoft.Identity.Client.Extensions.Msal
                         _logger.TraceEvent(TraceEventType.Error, /*id*/ 0, $"No data found in the store, clearing the cache in memory.");
 
                         // The cache is corrupt clear it out
-                        _store.Clear();
+                        Clear();
                         throw;
                     }
                 }
