f01d8524469d339fea8820f3279778b09ccf75e6
Mark Zuber <mark@zube.com>
2019-04-03 14:56:10 -0700

    Add claims extensions from identity web samples

---
+++ src/Microsoft.Identity.Client.Extensions.Web/ClaimConstants.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Identity.Client.Extensions.Web
+{
+    internal static class ClaimConstants
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ObjectId = "http://schemas.microsoft.com/identity/claims/objectidentifier";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TenantId = "http://schemas.microsoft.com/identity/claims/tenantid";
+    }
+}
