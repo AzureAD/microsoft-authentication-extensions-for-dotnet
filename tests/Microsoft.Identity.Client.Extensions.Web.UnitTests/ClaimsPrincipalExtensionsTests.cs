a190ab86e9071bb1250d4c1a26b897c7c22a79e4
Mark Zuber <mark@zube.com>
2019-04-08 13:19:23 -0700

    Add claims extensions from identity web samples (#17)

---
+++ tests/Microsoft.Identity.Client.Extensions.Web.UnitTests/ClaimsPrincipalExtensionsTests.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Security.Claims;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Client.Extensions.Web.UnitTests
+{
+    [TestClass]
+    public class ClaimsPrincipalExtensionsTests
+    {
+        [TestMethod]
+        public void EnsureGetMsalAccountIdReturnsProperValue()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+
+            string msalAccountId = principal.GetMsalAccountId();
+            Assert.AreEqual<string>($"{TestConstants.ObjectId}.{TestConstants.TenantId}", msalAccountId);
+        }
+
+        [TestMethod]
+        public void EnsureGetObjectIdReturnsProperValue()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+
+            string objectId = principal.GetObjectId();
+            Assert.AreEqual<string>(TestConstants.ObjectId, objectId);
+        }
+
+        [TestMethod]
+        public void EnsureGetTenantIdReturnsProperValue()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+
+            string tenantId = principal.GetTenantId();
+            Assert.AreEqual<string>(TestConstants.TenantId, tenantId);
+        }
+
+        [TestMethod]
+        public void EnsureGetDisplayNameIsNullWithSimpleClaimsPrincipal()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+
+            string displayName = principal.GetDisplayName();
+            Assert.IsNull(displayName);
+        }
+
+        [TestMethod]
+        public void EnsureGetLoginHintIsNullWithSimpleClaimsPrincipal()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+
+            string loginHint = principal.GetLoginHint();
+            Assert.IsNull(loginHint);
+        }
+
+        // TODO: add tests for ClaimsPrincipal created from IAccount instance...
+
+        [TestMethod]
+        public void EnsureGetDomainHintReturnsOrganizationsForNonMsaTenant()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+            string domainHint = principal.GetDomainHint();
+            Assert.AreEqual<string>("organizations", domainHint);
+        }
+
+        [TestMethod]
+        public void EnsureGetDomainHintReturnsConsumersForNonTenant()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.MsaTenantId, TestConstants.ObjectId);
+            string domainHint = principal.GetDomainHint();
+            Assert.AreEqual<string>("consumers", domainHint);
+        }
+
+        [TestMethod]
+        public void EnsureGetDomainHintReturnsNullForEmptyClaimsPrincipal()
+        {
+            ClaimsPrincipal principal = new ClaimsPrincipal();
+            string domainHint = principal.GetDomainHint();
+            Assert.IsNull(domainHint);
+        }
+    }
+}
---
+++ tests/Microsoft.Identity.Client.Extensions.Web.UnitTests/ClaimsPrincipalFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Security.Claims;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Client.Extensions.Web.UnitTests
+{
+    [TestClass]
+    public class ClaimsPrincipalFactoryTests
+    {
+        [TestMethod]
+        public void ValidateFromTenantIdAndObjectIdHaveExpectedClaims()
+        {
+            ClaimsPrincipal principal = ClaimsPrincipalFactory.FromTenantIdAndObjectId(TestConstants.TenantId, TestConstants.ObjectId);
+
+            Assert.IsTrue(principal.HasClaim(x => x.Type == "tid"));
+            Assert.IsTrue(principal.HasClaim(x => x.Type == "oid"));
+
+            var tidClaim = principal.FindFirst("tid");
+            var oidClaim = principal.FindFirst("oid");
+
+            Assert.AreEqual<string>(TestConstants.TenantId, tidClaim.Value);
+            Assert.AreEqual<string>(TestConstants.ObjectId, oidClaim.Value);
+        }
+    }
+}
