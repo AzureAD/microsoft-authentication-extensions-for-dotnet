6428a547b7cd8a00b102852287d7d232d565c410
David Justice <david@devigned.com>
2019-05-15 13:24:33 -0700

    add license header to each .cs

--- src/Microsoft.Identity.Client.Extensions.Msal/Providers/ExponentialBackoff.cs
+++ src/Microsoft.Identity.Client.Extensions.Msal/Providers/ExponentialBackoff.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client.Extensions.Msal.Providers.Exceptions;
