50512259ae6c3a489821f3f875276298262116a0
Bogdan Gavril <bogavril@microsoft.com>
2019-12-18 20:41:00 +0000

    Improve test app

---
+++ tests/ManualTestApp/Config.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Microsoft.Identity.Client.Extensions.Msal;
+
+namespace ManualTestApp
+{
+    public static class Config
+    {
+        // App settings
+        public static readonly string[] Scopes = new[] { "user.read" };
+
+        // Use "common" if you want to allow any "enterprise" (work or school) account AND any user account (live.com, outlook, hotmail) to log in.
+        // Use an actual tenant ID to allow only your enterprise to log in.
+        // Use "organizatins" to allow only enterprise log-in, this is required for the Username / Password flow
+        public const string Authority = "https://login.microsoftonline.com/organizations";
+        public const string ClientId = "1d18b3b0-251b-4714-a02a-9956cec86c2d";
+
+        // Cache settings
+        public const string CacheFileName = "msal_cache.dat";
+        public const string CacheDir = "MSAL_CACHE";
+
+        public const string KeyChainServiceName = "msal_service";
+        public const string KeyChainAccountName = "msal_account";
+
+        public const string LinuxKeyRingSchema = "com.contoso.devtools.tokencache";
+        public const string LinuxKeyRingCollection = MsalCacheStorage.LinuxKeyRingDefaultCollection;
+        public const string LinuxKeyRingLabel = "MSAL token cache for all Contoso dev tool apps.";
+        public static readonly KeyValuePair<string, string> LinuxKeyRingAttr1 = new KeyValuePair<string, string>("Version", "1");
+        public static readonly KeyValuePair<string, string> LinuxKeyRingAttr2 = new KeyValuePair<string, string>("ProductGroup", "MyApps");
+
+        // For Username / Password flow - to be used only for testing!
+        public const string Username = "";
+        public const string Password = "";
+
+    }
+}
--- tests/ManualTestApp/Program.cs
+++ tests/ManualTestApp/Program.cs
@@ -3,227 +3,238 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Drawing;
 using System.Linq;
-using System.Reflection;
+using System.Security;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.Extensions.Msal;
 
 namespace ManualTestApp
 {
-    public static class DefaultConfig
-    {
-        public const string Resource = "https://graph.microsoft.com/";
-        public const string TenantId = "49f548d0-12b7-4169-a390-bb5304d24462";
-        public static readonly Uri Environment = new Uri("https://login.microsoftonline.com");
-        public const bool ValidateAuthority = true;
-        public const string ClientId = "1d18b3b0-251b-4714-a02a-9956cec86c2d";
-        public const string CacheFileName = "msal.ext.cache";
-        public const string CacheDir = "msal_exp";
 
-        public const string KeyChainServiceName = "msal_service";
-        public const string KeyChainAccountName = "msal_account";
-
-        public const string LinuxKeyRingSchema = "schema";
-        public const string LinuxKeyRingCollection = "collection";
-        public const string LinuxKeyRingLabel = "label";
-        public static readonly KeyValuePair<string, string> LinuxKeyRingAttr1 = new KeyValuePair<string, string>("k1", "v1");
-        public static readonly KeyValuePair<string, string> LinuxKeyRingAttr2 = new KeyValuePair<string, string>("k2", "v2");
-    }
-
-    // Example usage:
-    // login-msal https://graph.microsoft.com/ 49f548d0-12b7-4169-a390-bb5304d24462 https://login.microsoftonline.com true 1d18b3b0-251b-4714-a02a-9956cec86c2d msal.ext.cache msal_exp
     class Program
     {
-        private static MsalCacheHelper s_helper;
 
 #pragma warning disable UseAsyncSuffix // Use Async suffix
         static async Task Main(string[] args)
 #pragma warning restore UseAsyncSuffix // Use Async suffix
         {
-            string input;
-            do
+
+            var pca = await CreatePublicClientWithCacheAsync().ConfigureAwait(false);
+            AuthenticationResult result;
+
+            while (true)
             {
-                Console.Write("> ");
-                input = Console.ReadLine();
-            } while (await RunCommandAsync(input).ConfigureAwait(true));
-        }
 
-        /// <summary>
-        /// Executes a user command
-        /// </summary>
-        /// <param name="splitInput">The command</param>
-        /// <returns>True if execution should continue, false if we should terminate.</returns>
-        private static async Task<bool> RunCommandAsync(string input)
-        {
-
-            var splitInput = input.Split(' ');
-            var command = splitInput[0].ToLowerInvariant();
-            var args = splitInput.Skip(1).ToArray();
-
-            switch (command)
-            {
-            case "quit":
-            case "exit":
-                return false;
-
-            case "help":
-                PrintUsage();
-                return true;
-
-            case "login-msal":
-                if (args.Length < 7)
+                //await DisplayAccountsAsync(pca).ConfigureAwait(false);
+                // Display menu
+                Console.WriteLine($@"
+                        1. Acquire Token using Username and Password - for TEST only, do not use in production!
+                        2. Acquire Token using Device Code Flow
+                        3. Acquire Token Interactive
+                        4. Acquire Token Silent
+                        5. Display Accounts (reads the cache)
+                        6. Acquire Token U/P and Silent in a loop
+                        c. Clear cache
+                        x. Exit app
+                    Enter your Selection: ");
+                char.TryParse(Console.ReadLine(), out var selection);
+                try
                 {
-                    Console.WriteLine("Incorrect format");
-                    PrintUsage();
+                    switch (selection)
+                    {
+                    case '1': //  Acquire Token using Username and Password (requires config)
+                        result = await AcquireTokenROPCAsync(pca).ConfigureAwait(false);
+                        DisplayResult(result);
+
+                        break;
+
+                    case '2': // Device Code Flow
+                        result = await pca.AcquireTokenWithDeviceCode(Config.Scopes, (dcr) =>
+                        {
+                            Console.BackgroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine(dcr.Message);
+                            Console.ResetColor();
+
+                            return Task.FromResult(1);
+                        }).ExecuteAsync().ConfigureAwait(false);
+                        DisplayResult(result);
+
+                        break;
+                    case '3': // Interactive
+                        result = await pca.AcquireTokenInteractive(Config.Scopes)
+                            .ExecuteAsync()
+                            .ConfigureAwait(false);
+                        DisplayResult(result);
+                        break;
+                    case '4': // Silent
+                        Console.WriteLine("Getting all the accounts. This reads the cache");
+                        var accounts = await pca.GetAccountsAsync().ConfigureAwait(false);
+                        var firstAccount = accounts.FirstOrDefault();
+
+                        // this is expected to fail when account is null
+                        result = await pca.AcquireTokenSilent(Config.Scopes, firstAccount)
+                            .ExecuteAsync()
+                            .ConfigureAwait(false);
+                        DisplayResult(result);
+                        break;
+                    case '5': // Display Accounts
+                        Console.Clear();
+                        var accounts2 = await pca.GetAccountsAsync().ConfigureAwait(false);
+                        if (!accounts2.Any())
+                        {
+                            Console.WriteLine("No accounts were found in the cache.");
+                        }
+
+                        foreach (var acc in accounts2)
+                        {
+                            Console.WriteLine($"Account for {acc.Username}");
+                        }
+                        break;
+                    case '6': // U/P and Silent in a loop
+                        Console.WriteLine("CTRL-C to stop...");
+
+                        while (true)
+                        {
+                            Console.WriteLine("Acquiring token by ROPC...");
+                            result = await AcquireTokenROPCAsync(pca).ConfigureAwait(false);
+                            Console.WriteLine("OK. Now getting the accounts");
+
+                            var accounts3 = await pca.GetAccountsAsync().ConfigureAwait(false);
+
+                            Console.WriteLine("Acquiring token silent");
+
+                            result = await pca.AcquireTokenSilent(Config.Scopes, accounts3.First())
+                                .ExecuteAsync()
+                                .ConfigureAwait(false);
+
+                            Console.WriteLine("Deleting the account");
+                            foreach (var acc in accounts3)
+                            {
+                                await pca.RemoveAsync(acc).ConfigureAwait(false);
+                            }
+
+                            Console.WriteLine("Waiting for 3 seconds");
+                            await Task.Delay(3000).ConfigureAwait(false);
+                        }
+
+                    case 'c':
+                        var accounts4 = await pca.GetAccountsAsync().ConfigureAwait(false);
+                        foreach (var acc in accounts4)
+                        {
+                            Console.WriteLine($"Removing account for {acc.Username}");
+                            await pca.RemoveAsync(acc).ConfigureAwait(false);
+                        }
+                        Console.Clear();
+
+                        break;
+                    
+                    case 'x':
+                        return;
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    await LoginWithCommandLineArgsAsync(args).ConfigureAwait(false);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Exception : " + ex);
+                    Console.ResetColor();
+                    Console.WriteLine("Hit Enter to continue");
+
+                    Console.Read();
                 }
-                return true;
-
-            case "login-msal-defaults":
-                await LoginWithDefaultsAsync().ConfigureAwait(false);
-                return true;
-            default:
-                Console.WriteLine("Unknown command");
-                PrintUsage();
-                return true;
             }
         }
 
-        private static async Task LoginWithDefaultsAsync()
+        private static async Task<AuthenticationResult> AcquireTokenROPCAsync(IPublicClientApplication pca)
         {
-            var storagePropertiesBuilder =
-                 new StorageCreationPropertiesBuilder(DefaultConfig.CacheFileName, DefaultConfig.CacheDir, DefaultConfig.ClientId)
-                 .WithLinuxKeyring(DefaultConfig.LinuxKeyRingSchema, DefaultConfig.LinuxKeyRingCollection, DefaultConfig.LinuxKeyRingLabel, DefaultConfig.LinuxKeyRingAttr1, DefaultConfig.LinuxKeyRingAttr2)
-                 .WithMacKeyChain(DefaultConfig.KeyChainServiceName, DefaultConfig.KeyChainAccountName);
-
-            await LoginInternalAsync(
-                DefaultConfig.Resource,
-                DefaultConfig.TenantId,
-                DefaultConfig.Environment,
-                DefaultConfig.ClientId,
-                storagePropertiesBuilder.Build()).ConfigureAwait(false);
-
-        }
-
-        private static async Task LoginWithCommandLineArgsAsync(string[] args) // No Linux support
-        {
-            try
+            if (string.IsNullOrEmpty(Config.Username) ||
+                string.IsNullOrEmpty(Config.Password))
             {
-                string resource = args[0];
-                string tenant = args[1];
-                Uri baseAuthority = new Uri(args[2]);
-#pragma warning disable CA1305 // Specify IFormatProvider
-                bool validateAuthority = Convert.ToBoolean(args[3]);
-#pragma warning restore CA1305 // Specify IFormatProvider
-                string clientId = args[4];
-                string cacheFileName = args[5];
-                string cacheDirectory = Path.Combine(MsalCacheHelper.UserRootDirectory, args[6]);
-
-                string serviceName = null;
-                string accountName = null;
-
-                var storagePropertiesBuilder =
-                    new StorageCreationPropertiesBuilder(cacheFileName, cacheDirectory, clientId);
-
-
-                if (args.Length > 7)
-                {
-                    serviceName = args[7];
-                    accountName = args[8];
-                    storagePropertiesBuilder.WithMacKeyChain(serviceName, accountName);
-                }
-
-                var storageProperties = storagePropertiesBuilder.Build();
-
-                await LoginInternalAsync(resource, tenant, baseAuthority, clientId, storageProperties).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error logging in with Msal: {ex}");
-                PrintUsage();
-            }
-        }
-
-        private static async Task LoginInternalAsync(string resource, string tenant, Uri baseAuthority, string clientId, StorageCreationProperties storageProperties)
-        {
-            (var scopes, var app, var helper) = await CreatePublicClientAndBindCacheAsync(
-                resource,
-                tenant,
-                baseAuthority,
-                clientId,
-                storageProperties
-                ).ConfigureAwait(false);
-
-            // The token cache helper's main feature is this event
-            if (s_helper == null)
-            {
-                s_helper = helper;
-                s_helper.CacheChanged += (s, e) =>
-                {
-                    Console.WriteLine($"Cache Changed, Added: {e.AccountsAdded.Count()} Removed: {e.AccountsRemoved.Count()}");
-                };
+                throw new InvalidOperationException("Please configure a username and password!");
             }
 
-            // Note: in a real app, you should ALWAYS call AcquireTokenSilent first!
-            var authResult = await app
-                .AcquireTokenInteractive(scopes)
+            SecureString securePassword = new SecureString();
+            foreach (char c in Config.Password)
+            {
+                securePassword.AppendChar(c);
+            }
+
+            var result = await pca.AcquireTokenByUsernamePassword(
+                Config.Scopes,
+                Config.Username,
+                securePassword)
                 .ExecuteAsync()
                 .ConfigureAwait(false);
 
-            var accounts = await app.GetAccountsAsync().ConfigureAwait(false);
-            foreach (var acc in accounts)
-            {
-                await app.AcquireTokenSilent(scopes, acc).ExecuteAsync().ConfigureAwait(false);
-                await app.RemoveAsync(acc).ConfigureAwait(false);
-            }
+            return result;
         }
 
-        private static void PrintUsage()
+        private static void DisplayResult(AuthenticationResult result)
         {
-            var usageString = @"USAGE: {quit,exit,help,login-msal, login-msal-defaults}
-login-msal takes args [resource tenant baseAuthority validateAuthority clientId cacheFileName cacheDirectory {macServiceName macAccountName}]";
-
-            Console.WriteLine(usageString);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Token Acquisition Success! Got a token for: " + result.Account.Username);
+            Console.WriteLine(result.AccessToken);
+            Console.ResetColor();
+            Console.WriteLine("Press ENTER to continue");
+            Console.Read();
+            Console.Clear();
         }
 
-        private static async Task<(string[], IPublicClientApplication, MsalCacheHelper)> CreatePublicClientAndBindCacheAsync(
-            string resource,
-            string tenant,
-            Uri baseAuthority,
+
+        private static async Task<IPublicClientApplication> CreatePublicClientWithCacheAsync()
+        {
+            var storagePropertiesBuilder =
+                 new StorageCreationPropertiesBuilder(Config.CacheFileName, Config.CacheDir, Config.ClientId)
+                 .WithLinuxKeyring(
+                     Config.LinuxKeyRingSchema,
+                     Config.LinuxKeyRingCollection,
+                     Config.LinuxKeyRingLabel,
+                     Config.LinuxKeyRingAttr1,
+                     Config.LinuxKeyRingAttr2)
+                 .WithMacKeyChain(
+                     Config.KeyChainServiceName,
+                     Config.KeyChainAccountName);
+
+            IPublicClientApplication pca = await CreatePublicClientAndBindCacheAsync(
+                Config.Authority,
+                Config.ClientId,
+                storagePropertiesBuilder.Build())
+                .ConfigureAwait(false);
+
+            return pca;
+        }
+
+        private static async Task<IPublicClientApplication> CreatePublicClientAndBindCacheAsync(
+            string authority,
             string clientId,
             StorageCreationProperties storageCreationProperties)
         {
-            // tenant can be null
-            resource = resource ?? throw new ArgumentNullException(nameof(resource));
 
-            Console.WriteLine($"Using resource: '{resource}', tenant:'{tenant}'");
-
-            var scopes = new string[] { resource + "/.default" };
-
-            Console.WriteLine($"Using scopes: '{string.Join(",", scopes)}'");
-
-            var authority = $"{baseAuthority.AbsoluteUri}{tenant}";
-            Console.WriteLine($"GetPublicClient for authority: '{authority}'");
-
-            Uri authorityUri = new Uri(authority);
             var appBuilder = PublicClientApplicationBuilder.Create(clientId)
-                .WithAuthority(authorityUri)
-                .WithRedirectUri("http://localhost");
+                .WithAuthority(authority)
+                .WithRedirectUri("http://localhost"); // make sure to register this redirect URI for the interactive login to work
+
             var app = appBuilder.Build();
             Console.WriteLine($"Built public client");
 
-            // This hooks up our custom cache onto the one used by MSAL
+            // This hooks up the cross-platform cache into  MSAL
             var cacheHelper = await MsalCacheHelper.CreateAsync(storageCreationProperties).ConfigureAwait(false);
             cacheHelper.RegisterCache(app.UserTokenCache);
 
             Console.WriteLine($"Cache registered");
 
-            return (scopes, app, cacheHelper);
+            // The token cache helper's main feature is this event
+            cacheHelper.CacheChanged += (s, e) =>
+            {
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine($"Cache Changed, Added: {e.AccountsAdded.Count()} Removed: {e.AccountsRemoved.Count()}");
+                Console.ResetColor();
+            };
+
+            Console.WriteLine($"Cache event wired up");
+
+            return app;
         }
     }
 }
