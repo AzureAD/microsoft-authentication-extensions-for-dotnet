731b2dd6d9cfac26d6012ec3fb63ed575b12d794
Bogdan Gavril <bogavril@microsoft.com>
2020-04-28 14:51:53 +0100

    cleanup

---
+++ tests/Microsoft.Identity.Client.Extensions.Msal.UnitTests/PerformanceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Identity.Client.Extensions.Msal.UnitTests
+{
+    class PerformanceTest
+    {
+    }
+}
